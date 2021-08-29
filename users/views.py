0a8a2764fa52701accb38f54c34dcd6bff9e7101
Alexey Polyantsev <alexpl92@gmail.com>
2021-08-29 19:34:40 +0300

    starting minor fixes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, update_session_auth_hash
+from djoser import utils
+from djoser.compat import get_user_email
+from djoser.conf import settings
+from djoser.views import UserViewSet as DjoserUserViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserViewSet(DjoserUserViewSet):
+
+    @action(['post'], detail=False)
+    def set_password(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        self.request.user.set_password(serializer.data['new_password'])
+        self.request.user.save()
+
+        if settings.PASSWORD_CHANGED_EMAIL_CONFIRMATION:
+            context = {"user": self.request.user}
+            to = [get_user_email(self.request.user)]
+            settings.EMAIL.password_changed_confirmation(
+                self.request,
+                context
+            ).send(to)
+
+        if settings.LOGOUT_ON_PASSWORD_CHANGE:
+            utils.logout_user(self.request)
+        elif settings.CREATE_SESSION_ON_LOGIN:
+            update_session_auth_hash(self.request, self.request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
